857fd6d6c32a07afd6215a6bb1ef52633dc921f5
liujuan <3003781852@qq.com>
2020-05-11 13:39:21 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router/router.js'
 import iview from 'iview'
+import tanikawaTis from 'tanikawa-tis'
 import store from "./store/index.js"
 import 'font-awesome/css/font-awesome.css'; //font-awesome图标库
 import  "./theme/index.less"
@@ -9,12 +10,11 @@ import  "./theme/front_standard.less"
 import 'viewerjs/dist/viewer.css'  //v-viewer大图预览
 import Viewer from 'v-viewer'
 
-
 Vue.config.productionTip = false
-
+Vue.use(tanikawaTis);
 Vue.use(iview)
 Vue.use(Viewer);
-
+console.log(tanikawaTis,iview)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
