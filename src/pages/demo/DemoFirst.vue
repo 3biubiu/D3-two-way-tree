b74432376d4d40e3c1485d860da7a630d184170b
liujuan <3003781852@qq.com>
2020-06-15 09:10:54 +0800

    add

--- src/pages/demo/DemoFirst.vue
+++ src/pages/demo/DemoFirst.vue
@@ -3,6 +3,24 @@
         士大夫的考生放射科大夫
         <tis-demo-one></tis-demo-one>
         <tis-demo-two></tis-demo-two>
+        <br/>
+        <br/>
+        <div class="tis-btn primary-border">大夫</div>
+        <div class="tis-btn primary-bg">大夫</div>
+        <div class="tis-btn primary-bg"><i class="iconfont iconedit"></i>大夫</div>
+        <div class="tis-btn primary-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
+
+        <div class="tis-btn success-bg">大夫</div>
+        <div class="tis-btn success-bg"><i class="iconfont iconedit"></i>大夫</div>
+        <div class="tis-btn success-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
+
+        <div class="tis-btn warn-bg">大夫</div>
+        <div class="tis-btn warn-bg"><i class="iconfont iconedit"></i>大夫</div>
+        <div class="tis-btn warn-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
+        
+        <div class="tis-btn error-bg">大夫</div>
+        <div class="tis-btn error-bg"><i class="iconfont iconedit"></i>大夫</div>
+        <div class="tis-btn error-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
     </div>
 </template>
 <script>
