5fb1010df60c7ba5aa07ad2fca00833ee31d7bdf
liujuan <3003781852@qq.com>
2020-06-15 11:58:30 +0800

    button

--- src/pages/demo/DemoFirst.vue
+++ src/pages/demo/DemoFirst.vue
@@ -4,23 +4,35 @@
         <tis-demo-one></tis-demo-one>
         <tis-demo-two></tis-demo-two>
         <br/>
-        <br/>
         <div class="tis-btn primary-border">大夫</div>
         <div class="tis-btn primary-bg">大夫</div>
         <div class="tis-btn primary-bg"><i class="iconfont iconedit"></i>大夫</div>
-        <div class="tis-btn primary-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
+        <div class="tis-btn primary-bg" disabled-bg><i class="iconfont iconedit"></i>大夫</div>
+        <div class="tis-btn primary-bg" disabled-border><i class="iconfont iconedit"></i>大夫</div>
+        <br/>
 
+        <div class="tis-btn success-border">大夫</div>
         <div class="tis-btn success-bg">大夫</div>
         <div class="tis-btn success-bg"><i class="iconfont iconedit"></i>大夫</div>
         <div class="tis-btn success-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
+        <br/>
 
+        <div class="tis-btn warn-border">大夫</div>
         <div class="tis-btn warn-bg">大夫</div>
         <div class="tis-btn warn-bg"><i class="iconfont iconedit"></i>大夫</div>
         <div class="tis-btn warn-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
+        <br/>
         
+        <div class="tis-btn error-border">大夫</div>
         <div class="tis-btn error-bg">大夫</div>
         <div class="tis-btn error-bg"><i class="iconfont iconedit"></i>大夫</div>
         <div class="tis-btn error-bg" disabled><i class="iconfont iconedit"></i>大夫</div>
+        <br/>
+
+        <div class="tis-btn default-primary">大夫</div>
+        <div class="tis-btn default-success">大夫</div>
+        <div class="tis-btn default-warn">大夫</div>
+        <div class="tis-btn default-error">大夫</div>
     </div>
 </template>
 <script>
