14ad31de6d157589c8abe722d68d6dc752ef58d4
wumingqin <wumingqin@zhaoshang.net>
2022-11-07 11:29:18 +0800

    优化前端框架

--- src/components/watermark/Watermark.vue
+++ src/components/watermark/Watermark.vue
@@ -4,58 +4,62 @@
 <script>
 import Cookies from 'js-cookie';
 export default {
-    // name:'water-mark',
-    data(){
-        return{
-            name:Cookies.get('username')?Cookies.get('username'):'',
-            img:localStorage.getItem('nameImg'+Cookies.get('uid'))
-        }
-    },
-    mounted(){
-        if(!this.img){
-            this.againImg(this.name);
-        }
-    },
-    methods:{
-        /**
-         * 没有存上图的情况
-         * @param {string} name 生成的名字
-         */
-        againImg(name){
-            if(!this.img && name){
-                this.setWatermark(name)
-                this.img = localStorage.getItem('nameImg'+Cookies.get('uid'));
-            }
-        },
-        /**
-         * 生成背景图
-         * @param {string} str 生成的名字
-         */
-        setWatermark(str){
-            //创建一个画布
-            let can = document.createElement('canvas');
-            //设置画布的长宽
-            can.width = 600+str.length*16;
-            can.height = str.length*16*20 * Math.PI / 180+400;
-
-            let cans = can.getContext('2d');
-            //旋转角度
-            cans.rotate(-20 * Math.PI / 180);
-            cans.font = '16px Vedana';
-            //设置填充绘画的颜色、渐变或者模式
-            cans.fillStyle = 'rgba(200, 200, 200, 0.40)';
-            //设置文本内容的当前对齐方式
-            cans.textAlign = 'left';
-            //设置在绘制文本时使用的当前文本基线
-            cans.textBaseline = 'Middle';
-            //在画布上绘制填色的文本（输出的文本，开始绘制文本的X坐标位置，开始绘制文本的Y坐标位置）
-            cans.fillText(str, can.width / 16, can.height / 4);
-            cans.fillText(str, can.width/2-can.width / 16, 7*can.height / 8);
-            localStorage.setItem('nameImg'+Cookies.get('uid'),can.toDataURL('image/png'))
-        }
+  // name:'water-mark',
+  data() {
+    return {
+      name: Cookies.get("username") ? Cookies.get("username") : "",
+      img: localStorage.getItem("waterMarkImg" + Cookies.get("uid")),
+    };
+  },
+  mounted() {
+    if (!this.img) {
+      this.againImg(this.name);
     }
-
-}
+  },
+  methods: {
+    /**
+     * 没有存上图的情况
+     * @param {string} name 生成的名字
+     */
+    againImg(name) {
+      // document.getElementsByClassName('watermark')[0].style.left = document.getElementsByClassName('sidebar-menu-con') != null ? "248px" : "0px"
+      if (!this.img) {
+        this.setWatermark(name);
+        this.img = localStorage.getItem("waterMarkImg" + Cookies.get("uid"));
+      }
+    },
+    /**
+     * 生成背景图
+     * @param {string} str 生成的名字
+     */ setWatermark(str) {
+      //特殊name的截取处理， 只保留名
+      if (str.indexOf("·") != -1) {
+        str = str.slice(0, str.indexOf("·"));
+      } // 创建一个画布
+      let can = document.createElement("canvas"); //设置画布的长宽
+      can.width = 280 + 10 * str.length;
+      can.height = 280 + 10 * str.length; //设置旋转角度
+      let angle = (-20 * Math.PI) / 180; //设置文字排列的单位距离
+      let strWidth = can.width / 4;
+      let strHeight = can.height / 4; //建立一个二维渲染上下文
+      let cans = can.getContext("2d"); // 设置旋转原点
+      cans.translate(can.width / 2, can.height / 2); //旋转角度
+      cans.rotate((-20 * Math.PI) / 180);
+      cans.font = "14px Vedana"; //设置填充绘画的颜色、渐变或者模式
+      cans.fillStyle = "rgba(158, 158, 158,0.20)"; //设置文本内容的当前对齐方式
+      cans.textAlign = "center"; //设置在绘制文本时使用的当前文本基线
+      cans.textBaseline = "Middle"; //通过旋转坐标系公式计算文字的相对位置
+      let x = strWidth * Math.cos(angle) - strHeight * Math.sin(angle);
+      let y = strWidth * Math.sin(angle) + strHeight * Math.cos(angle); //在画布上绘制填色的文本（输出的文本，开始绘制文本的X坐标位置，开始绘制文本的Y坐标位置）
+      cans.fillText(str, -x, y);
+      cans.fillText(str, x, -y);
+      localStorage.setItem(
+        "waterMarkImg" + Cookies.get("uid"),
+        can.toDataURL("image/png")
+      );
+    },
+  },
+};
 </script>
 <style scoped lang='less'>
     .watermark{
@@ -65,6 +69,6 @@ export default {
         left: 0;
         top: 0;
         pointer-events: none;
-        z-index: 99;
+        z-index: 9999;
     }
 </style>
