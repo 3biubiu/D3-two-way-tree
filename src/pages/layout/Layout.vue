34f4fc7d3647e87b042999b2fd73c61c63938682
lixiaowei <lixiaowei@zhaoshang.net>
2022-11-11 18:03:43 +0800

    bug

--- src/pages/layout/Layout.vue
+++ src/pages/layout/Layout.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="main">
         <header-menu
-            routerName="test"
+            :powerHeaderData="headerPower"
+            :commonHeaderUrl="commonHeaderUrl"
+            :routerName="routerName"
+            :systemName="systemName"
+            @get-name="drawName"
+            @shrink-menu="shrinkMenu"
             >
         </header-menu>
-        <shrinkable-menu routerName="test"></shrinkable-menu>
+        <shrinkable-menu
+            :systemName="systemName"
+            :routerName="routerName"
+            :left-tips="leftTips"
+            :power="userPower"
+            ></shrinkable-menu>
         <div class="single-page-con" id="js-main-content"  ref="pageBody" :style="{marginLeft: shrink?'64px':'220px'}">
             <div class="single-page" >
                 <router-view ref="view"/>
@@ -42,13 +52,16 @@
         },
         data () {
             return {
+                commonHeaderUrl: Config.apiUrl + '/system/header-info',
                 routerName:'test',//当前路由名
+                systemName:'test',//当前路由名
+                leftTips:{},// 未读消息数对象
                 menuList: [],//在左侧显示的列表
                 menuTitle: '',//在左侧显示的列表
                 shrink: false,//菜单收缩
                 menuTheme:'dark',//菜单主题
-                powerSiderData:[],  // 侧边栏权限数据
-                powerHeaderData:[],  //头部权限数据
+                // powerSiderData:[],  // 侧边栏权限数据
+                // powerHeaderData:[],  //头部权限数据
                 ucmsOn: false,
                 isInternalUser: false,//灰度发布用户
                 ucmsIframeSrc: '',//续用户中心生命周期
@@ -61,7 +74,7 @@
             }
         },
         computed: {
-            ...mmsCommon.mapGetters(["sideBarNumber"]),
+            ...mmsCommon.mapGetters(["sideBarNumber","headerPower", "userPower"]),
         },
         watch: {},
         created () {
@@ -76,6 +89,22 @@
             this.ucmsIframeEvent();
         },
         methods: {
+            /**
+             * 画水印图
+             */
+            drawName(){
+                this.$refs.waterMark.againImg(Cookie.get('username'))
+            },
+            /**
+             * 菜单收缩控制
+             */
+            shrinkMenu(shrink){
+                this.shrink = shrink;
+                // //保证产业系统瀑布流自适应事件自动响应
+                // this.$nextTick(()=>{
+                //     window.dispatchEvent(new Event('resize'));
+                // });
+            },
             /*
                 初始化layout数据：左侧未读条数，头部数据，权限数据。。。
             */
